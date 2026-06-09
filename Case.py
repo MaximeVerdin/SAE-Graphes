464644b24bbb12e53f8adcd764af5a7f2aa3971a
MaximeVerdin <maxime.verdin@etu.univ-littoral.fr>
2026-06-09 16:15:51 +0200

    modify: Case.py -> __atr

--- Case.py
+++ Case.py
@@ -2,28 +2,28 @@ class Case:
     def __init__(self, x: int, y: int, c: int = 0) -> None:
         '''Méthode dédiée, constructeur de la classe'''
 
-        self.abscisse: int = x
-        self.ordonne: int = y
-        self.contenu : int | None = None if c == 0 else c
+        self.__abscisse: int = x
+        self.__ordonne: int = y
+        self.__contenu : int | None = None if c == 0 else c
 
 
     def setContenu(self, cakechose: int) -> None:
-        '''Méthode publique, affecte le contenu de l'objet.'''
-        self.contenu = cakechose
+        '''Méthode publique, affecte le __contenu de l'objet.'''
+        self.__contenu = cakechose
 
 
     def getContenu(self) -> int:
-        '''Méthode publique, renvoie le contenu de l'objet.'''
-        return self.contenu
+        '''Méthode publique, renvoie le __contenu de l'objet.'''
+        return self.__contenu
 
 
     def getPosition(self) -> tuple:
         '''Méthode publique, renvoie la position de l'objet : tuple (x, y)'''
-        return (self.__abscisse, self.ordonne)
+        return (self.__abscisse, self.__ordonne)
 
     def toList(self) -> list:
         '''
         Retourne une liste de l'objet
         :return: (x, y, valeur)
         '''
-        return [self.abscisse, self.ordonne, self.contenu]
+        return [self.__abscisse, self.__ordonne, self.__contenu]
